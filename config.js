499c8a9717de139235d99529bdef97e65e5e8160
acjaguirre <adrian@xygen.io>
2023-07-13 13:13:58 -0300

    Primer Commit

---
+++ config.js
@@ -0,0 +1,7 @@
+export const config = {
+  database: 'qrefy',
+  username: 'root',
+  password: 'Xygen.io',
+  host: 'localhost',
+  dialect: 'mysql',
+};
---
+++ index.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import cors from 'cors';
+import sequelize from './database.js';
+import User from './models/User.js'; 
+import routes from './routes/auth.js';
+
+const app = express();
+
+
+// Middleware para permitir CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// Configuración de CORS
+app.use(cors());
+
+// Configuración de Express
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Rutas de la API
+app.use('/api', routes);
+
+/* sequelize.authenticate(); */
+
+// Sincronizar el modelo con la base de datos
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('Modelos sincronizados con la base de datos.');
+    // Iniciar el servidor
+    const port = 3000;
+    app.listen(port, () => {
+      console.log(`Servidor iniciado en http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error al sincronizar los modelos:', error);
+  });
+
+
+
+
+
