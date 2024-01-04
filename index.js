0da1ed2824a60ccbf74121ad974f6a8c2b5b765b
Lisandro <lisandro@xygen.io>
2024-01-03 23:38:15 -0300

    Added: añadiendo csrf y cookie parser

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import {sequelize, test_connection} from './models/database.js';
 import routes from './routes/users_api.js';
+import csrf from 'csurf';
 import jwt_token from './routes/jwt_token.js';
 import validateToken from './middleware/jwt_token_middleware.js';
 
 const app = express();
 
-// Middleware para permitir CORS
+// Middleware para permitir CORS y CSRF
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+let csrfMiddleware = csrf({cookie: true});
 
-// Configuración de CORS
+// Configuración de CORS y CSRF
+app.use(cookieParser());
+app.use(csrfMiddleware);
 app.use(cors());
 app.disable('x-powered-by')
 
