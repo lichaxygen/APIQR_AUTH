d60ded080cbaffce88b00876e2d7fcbe99bb6a46
Lisandro Pintos <lisandro@xygen.io>
2024-01-07 19:32:47 -0300

    cambiando el nombre de la carpeta a uno mas apropiado

--- src/controllers/jwt_token/jwt_token_controller.ts
+++ src/controllers/jwt_token/jwt_token_controller.ts
@@ -1,4 +1,4 @@
-import { validate_body } from "../../schemas/token_validator.js";
+import { validate_body } from "../../zod_validators/token_validator.js";
 import bcrypt from 'bcrypt';
 import config from "../../../config.js";
 import jwt, { Secret } from 'jsonwebtoken';
